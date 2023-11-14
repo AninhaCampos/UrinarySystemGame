355845d79151dc94f4e6993f031d3941b7f5abeb
p33dro <133718489+Pedro867@users.noreply.github.com>
2023-11-14 12:16:21 -0300

    Add files via upload

--- Page_inicio/config.js
+++ Page_inicio/config.js
@@ -1,6 +1,36 @@
-const Overlay = document.getElementById('.config');
+const Overlay = document.getElementById('overlay');
+const Char = document.getElementById('boneco');
+const Baloon = document.getElementById('balao');
+const Bastoes = document.getElementById('boneco');
+const Bastoes2 = document.getElementById('balao');
+const saida = document.getElementById('sair');
+/*const sistema = document.getElementById('titulo')*/;
+
 
 function callConfig(){
+    Overlay.style.visibility = "visible";
+    Bastoes.style.visibility = "hidden";
+    Bastoes2.style.visibility = "hidden";
+    saida.style.visibility = "visible";
+}
+
+function exitConfig(){
     Overlay.style.visibility = "hidden";
-    /*How.style.visibility = "visible";*/
+    Bastoes.style.visibility = "visible";
+    Bastoes2.style.visibility = "visible";
+    saida.style.visibility = "hidden";
+}
+
+function callCredits(){
+    Overlay.style.visibility = "visible";
+    Bastoes.style.visibility = "hidden";
+    Bastoes2.style.visibility = "hidden";
+    saida.style.visibility = "visible";
+}
+
+function exitCredits(){
+    Overlay.style.visibility = "hidden";
+    Bastoes.style.visibility = "visible";
+    Bastoes2.style.visibility = "visible";
+    saida.style.visibility = "hidden";
 }
