5886a387f0c0fd43334f9da3ef695b3dd04c218d
Ana Luiza <125516321+AninhaCampos@users.noreply.github.com>
2023-11-15 21:10:12 -0300

    Add files via upload
    perguntas adicionadas.

--- Trabalho de Biologia/arquivos.js/quiz.js
+++ Trabalho de Biologia/arquivos.js/quiz.js
@@ -1,55 +1,107 @@
 const questions = [
     {
-        question: "Qual é o maior grupo indígena do Brasil?",
-        options: ["Ticuna","Guarani","Guajajara","Yanomami"],
-        optionCorreta: "Guarani",
+        question: "A urina é um líquido que tem a função de excretar substâncias do nosso corpo. Sua composição é 95% água e 5% de resíduos dissolvidos, como ureia, amônia, ácido úrico e sais minerais.A sequência correta que indica as etapas de formação de urina no organismo é:",
+        options: ["Solubilização, Filtração, Reabsorção e Eliminação.","Secreção, Solubilização, Filtração e Excreção.","Filtração, Reabsorção, Secreção e Excreção.","Eliminação, Concentração, Filtração e Excreção."],
+        optionCorreta: "Filtração, Reabsorção, Secreção e Excreção.",
     },
     {
-        question: "Qual é o nome do sistema de troca adotado pelos portugueses com os indígenas no início da colonização?",
-        options: ["escambo","encomienda","repartimento","mita"],
-        optionCorreta: "escambo",
+        question: "Sobre o sistema urinário feminino e masculino é correto afirmar que:",
+        options: ["O sistema urinário feminino e masculino são idênticos.","O sistema urinário feminino e masculino diferem na quantidade de vias urinárias.","O sistema urinário feminino e masculino diferem no tamanho da uretra.","O sistema urinário feminino e masculino diferem no tamanho da uretra.","O sistema urinário feminino e masculino apresentam as mesmas funções."],
+        optionCorreta: "O sistema urinário feminino e masculino diferem no tamanho da uretra.",
     },
     {
-        question: "Qual é o nome do principal grupo indígena que apoiou os franceses na tentativa destes de colonizar terras do Brasil?",
-        options: ["Tupinambás","Tamoio","Guarani","Pataxó"],
-        optionCorreta: "Tamoio",
+        question: "Qual a função do sistema urinário no organismo humano?",
+        options: ["Produção de hormônios","Digestão de alimentos", "Eliminação de resíduos metabólicos", "Respiração celular"],
+        optionCorreta: "Eliminação de resíduos metabólicos",
     },
     {
-        question: "Quantos grupos étnicos indígenas existem no Brasil atualmente?",
-        options: ["266","400","257","305"],
-        optionCorreta: "305",
+        question: "Quais são os órgãos principais do sistema urinário?",
+        options: ["Fígado e vesícula biliar","Pulmões e traqueia","Coração e vasos sanguíneos","Rins e bexiga"],
+        optionCorreta: "Rins e bexiga",
     },
     {
-        question: "Quais eram as principais mercadorias desejadas pelos indígenas no sistema de escambo?",
-        options: ["alimentos","espelhos","objetos metálicos","cavalos"],
-        optionCorreta: "objetos metálicos",
+        question: "O que é a filtração glomerular e qual sua importância no sistema urinário?",
+        options: ["Processo de formação da urina nos rins","Transporte da urina do rim até a bexiga","Produção de hormônios renais","Eliminação de resíduos pela pele"],
+        optionCorreta: "Processo de formação da urina nos rins",
     },
     {
-        question: "Qual é a capital do Brasil onde se encontra a sede da Fundação Nacional do Índio (FUNAI)?",
-        options: ["Rio de Janeiro","Belo Horizonte","Brasília","Manaus"],
-        optionCorreta: "Brasília",
+        question: "Como ocorre a formação da urina nos rins?",
+        options: ["Através da ingestão de líquidos","Por meio da digestão dos alimentos","Pela filtração do sangue nos néfrons renais","Pela produção de enzimas no fígado"],
+        optionCorreta: "Pela filtração do sangue nos néfrons renais",
     },
     {
-        question: "Qual é a língua indígena mais falada no Brasil atualmente?",
-        options: ["Tikuna","Mundurukú","Guarani","Macro-Jê"],
-        optionCorreta: "Tikuna",
-        explication: "Embora exista uma grande diversidade de grupos e línguas indígenas diferentes, a língua mais falada é o Tikuna (pertencente a outros troncos línguisticos) com 34 mil falantes."
+        question: " Quais são as principais substâncias presentes na urina?",
+        options: ["Glicose e proteínas","Hemácias e plaquetas","Água e sais minerais","Vitaminas e minerais"],
+        optionCorreta: "Água e sais minerais",
     },
     {
-        question: "Qual é o tronco-linguístico indígena mais conhecido?",
-        options: ["Tupi","Mundurukú","Mawé","Macro-Jê"],
-        optionCorreta: "Tupi",
+        question: "Como ocorre o transporte da urina do rim até a bexiga?",
+        options: ["Através dos ureteres","Pelo canal uretral","Pela uretra","Por meio dos vasos sanguíneos"],
+        optionCorreta: "Através dos ureteres",
     },
     {
-        question: "Quem é o líder indígena que discursou a favor dos povos originários na assembleia constituinte?",
-        options: ["Raoni Metuktire","Davi Kopenawa","Daniel Munduruku","Ailton Krenak"],
-        optionCorreta: "Ailton Krenak",
+        question: "Quais são os problemas de saúde mais comuns relacionados ao sistema urinário?",
+        options: ["Diabetes e hipertensão","Asma e bronquite","Câncer de pulmão e enfisema","Infecção urinária e cálculo renal"],
+        optionCorreta: "Infecção urinária e cálculo renal",
     },
     {
-        question: "Qual é o(a) lider indígena ministro(a) do Ministério dos Povos Indígenas?",
-        options: ["Ailton Krenak","Sõnia Guajajara","Daniel Munduruku","Daiara Tukano"],
-        optionCorreta: "Sônia Guajajara",
-    }
+        question: "Qual é a diferença entre infecção urinária e cálculo renal?",
+        options: ["A infecção urinária é causada por bactérias, enquanto o cálculo renal é a formação de pedras nos rins","A infecção urinária afeta os pulmões, enquanto o cálculo renal afeta o fígado","A infecção urinária é uma doença viral, enquanto o cálculo renal é causado por alergias","A infecção urinária é uma condição genética, enquanto o cálculo renal é causado por má alimentação"],
+        optionCorreta: "A infecção urinária é causada por bactérias, enquanto o cálculo renal é a formação de pedras nos rins",
+    },
+    {
+        question:"Quais são os fatores de risco para o desenvolvimento de doenças renais?",
+        options: ["Consumo excessivo de água e falta de atividade física","Exposição ao sol e falta de sono","Tabagismo e consumo excessivo de álcool","Diabetes e hipertensão"],
+        optionCorreta: "Diabetes e hipertensão",
+    },
+    {
+        question: "Como é possível manter a saúde do sistema urinário?",
+        options: ["Praticando atividade física regularmente","Consumindo alimentos ricos em gordura saturada","Evitando o consumo de água","Mantendo uma boa hidratação e adotando hábitos alimentares saudáveis"],
+        optionCorreta: "Mantendo uma boa hidratação e adotando hábitos alimentares saudáveis",
+    },
+    {
+        question: "Indique qual das alternativas a seguir NÃO apresenta uma função do sistema urinário.",
+        options: ["Filtração do sangue para remoção de impurezas","Condução, armazenamento e excreção da urina","Manutenção da concentração de água e sais minerais no organismo","Regulação da quantidade de sangue que circula no corpo."],
+        optionCorreta: "Regulação da quantidade de sangue que circula no corpo.",
+    },
+    {
+        question: "Sistema Urinário: Em caso de hipertensão, recomenda-se uma dieta sem sal, porque este atua:",
+        options: ["Diminuindo o volume de sangue circulante.","Aumentando o volume de sangue circulante.","Reduzindo o calibre dos vasos sanguíneos.","Dilatando o calibre dos vasos sanguíneos.","Obstruindo os capilares arteriais com placas de ateroma."],
+        optionCorreta: "Aumentando o volume de sangue circulante.aumentando o volume de sangue circulante.",
+    },
+    {
+        question: "Uma pessoa passará a excretar maior quantidade de uréia se aumentar, em sua dieta alimentar, a quantidade de:",
+        options: ["amido.","cloreto de sódio.","glicídios.","lipídios.","proteínas."],
+        optionCorreta: "proteínas.",
+    },
+    
+    {
+        question: "“As fezes e auréia são resíduos produzidos  pelos mamíferos. Entretanto somente um deles é considerado excreção”. Escolha a alternativa que condiz com este enunciado:",
+        options: ["as fezes, porque resultam da atividade de absorção deágua pelo intestino grosso.","a ureia, porque é produzida por células flama, as quais são encontradas nos rins dos mamíferos.","as fezes, porque resultam da ação da flora intestinal.","a ureia, por ser uma substância nitrogenada tóxica produzida durante o metabolismo celular."],
+        optionCorreta: "Infecção urinária e cálculo renal",
+    },
+  
+    {
+        question:"Os rins artificiais são aparelhos utilizados por pacientes com distúrbios renais. A função desses aparelhos é:" ,
+        options: ["oxigenar o sangue desses pacientes, uma vez que uma menor quantidade de gás oxigênio é liberada em sua corrente sanguínea.","nutrir o sangue desses pacientes, uma vez que sua capacidade de absorver nutrientes orgânicos está diminuída","retirar o excesso de gás carbônico que se acumula no sangue desses pacientes.","retirar o excesso de glicose, proteínas e lipídios que se acumula no sangue desses pacientes.","retirar o excesso de íons e resíduos nitrogenados que se acumula no sangue desses pacientes."],
+        optionCorreta: "retirar o excesso de íons e resíduos nitrogenados que se acumula no sangue desses pacientes.",
+    },
+    /*
+    {
+        question: ,
+        options: ["Diabetes e hipertensão","Asma e bronquite","Câncer de pulmão e enfisema","Infecção urinária e cálculo renal"],
+        optionCorreta: "Infecção urinária e cálculo renal",
+    },
+    {
+        question: ,
+        options: ["Diabetes e hipertensão","Asma e bronquite","Câncer de pulmão e enfisema","Infecção urinária e cálculo renal"],
+        optionCorreta: "Infecção urinária e cálculo renal",
+    },
+    {
+        question: ,
+        options: ["Diabetes e hipertensão","Asma e bronquite","Câncer de pulmão e enfisema","Infecção urinária e cálculo renal"],
+        optionCorreta: "Infecção urinária e cálculo renal",
+    },*/
 ];
 
 let cont_perg = 0;
